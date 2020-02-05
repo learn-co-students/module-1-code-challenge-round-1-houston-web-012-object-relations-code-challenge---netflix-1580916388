60321476acc5804111e97082cef39d9bdaa9b5c6
github username <github email address>
2020-02-05 10:53:31 -0600

    blake first commit

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,28 @@
 class Review
 
+    attr_accessor :rating
+    attr_reader :viewer, :movie
+
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def viewers
+        self.viewer
+    end
+
+    def movies
+        self.movie
+    end
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,23 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def reviews
+    Review.all.select{|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.map{|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+    
   end
   
 end
