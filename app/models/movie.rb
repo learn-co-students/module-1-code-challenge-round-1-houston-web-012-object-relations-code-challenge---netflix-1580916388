aa6c48adb2783ef7218e503185751312921bb363
corn-cloud <comowaiye@gmail.com>
2020-02-05 10:48:15 -0600

    VICTORY!

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,33 @@ class Movie
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
+
+
+
+  def reviews
+    Review.all.select {|m| m.movie == self}
+  end
+
+  def reviewers
+    reviews.map {|r| r.viewer}
+  end 
+
+  def average_rating
+    reviews.sum {|a| a.rating} / reviews.size
+  end
+
+  def self.highest_rated
+    self.all.find {|m| m.reviews.max_by}
+  end
+
+  
+
+  
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,28 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select {|v| v.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.map {|v| v.movie}
+  end
+
+  def rate_movie(movie, rating)
+    Review.new(self, movie, rating)
+  end
+
+
+
+
+
   
 end
