9cb87a5c28d323bd1ca3d79466ec8fc1668211e4
vienpham2019 <vienpham2019@gmail.com>
2020-02-05 10:55:22 -0600

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,15 +1,34 @@
 class Viewer
-  attr_accessor :username
+  attr_reader :username
 
   @@all = []
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    self.class.all << self #@@all 
   end
 
   def self.all
     @@all
   end
-  
+
+  def reviews 
+    Review.all.select{|review| review.viewer == self}
+  end
+
+  def reviewed_movies 
+    reviews.map{|review| review.movie}
+  end
+
+  def reviewed_movies?(movie)
+    !!reviewed_movies.find{|viewer_movie| viewer_movie == movie}
+  end
+
+  def rate_movie(movie,rating)
+    if reviewed_movies.include?(movie)
+      reviews.find{|review| review.movie == movie}.rating = rating
+    else
+      Review.new(self,movie,rating)
+    end
+  end
 end
