17e7fae9271e21a0947b964fc7114a2a6fd38544
anamsoomro <anamsoomroed@gmail.com>
2020-02-05 10:34:51 -0600

    submission

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,35 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews 
+    Review.all.select {|rev| rev.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.map {|rev| rev.movie}
+  end
+
+  def reviewed_movie? (target_movie)
+    reviewed_movies.include? target_movie
+  end
+
+  def find_review (movie)
+    reviews.find {|rev| rev.movie == movie}
+  end
+
+  def rate_movie (movie, rating)
+    if reviewed_movie?(movie)
+      find_review(movie).rating = rating
+    else 
+      Review.new(self, movie, rating)
+    end
+  end
   
 end
+
+
+
+
+
+
