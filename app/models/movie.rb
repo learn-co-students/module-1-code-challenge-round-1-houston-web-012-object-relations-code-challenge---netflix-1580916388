6abbd9c4b4fb2e1b7d42fb843f621d27dfe580ff
timothyalton <mitchell.alton@live.com>
2020-02-05 10:56:27 -0600

    mod 1 code challenge

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,35 @@ class Movie
 
   def initialize(title)
     @title = title
-    self.class.all << self
+
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def reviews
+    Review.all.select {|review| review.movie == self}
+  end
+
+  def reviewers
+    reviews.map {|review| review.viewer}.uniq # uniq wouldnt really be necessary for this scenario... 
+  end
+
+  def average_rating
+    reviews.sum {|review| review.rating} / reviews.count
+  end
+
+  def self.highest_rated
+    # @@all.each do |movie|
+    #   movie.reviews if review.rating.max
+    #   return movie 
+    # end # doesn't work 
+    # @@all.select {|movie| movie.reviews if review.rating.max} #doesn't work
+
+    @@all.select {|movie| movie.reviews}
+
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,6 +10,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+m1 = Movie.new("Star Wars")
+m2 = Movie.new("The Avengers")
+m3 = Movie.new("Indiana Jones")
+m4 = Movie.new("The Notebook")
+
+v1 = Viewer.new("SappyGirl")
+v2 = Viewer.new("JediMaster")
+v3 = Viewer.new("StrongestAvenger")
+v4 = Viewer.new("No Hat Left Behind")
+
+r1 = Review.new(v1, m1, 7)
+r2 = Review.new(v2, m1, 10)
+r3 = Review.new(v3, m2, 10)
+r4 = Review.new(v1, m4, 8)
+r5 = Review.new(v4, m3, 10)
+r6 = Review.new(v1, m3, 5)
+r7 = Review.new(v2, m4, 4)
+
+
 
 
 
