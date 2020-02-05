5868147ac9a652274a18b79441033062768cdd0b
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-05 10:57:19 -0600

    Boom!

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,33 @@ class Movie
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def reviews
+    Review.all.select{|review| review.movie == self}
+  end
+
+  def reviewers
+    reviews.map{|review| review.viewer}.uniq
+  end
+
+  def average_rating
+    total_rating = reviews.sum{|review| review.rating}
+    average = total_rating.to_f/reviews.count
+    average
+  end
+
+  def avgrating
+    reviews.sum{|review| review.rating}.to_f/reviews.count
+  end
+
+  def self.highest_rated
+    all.max_by{|movie| movie.average_rating}
   end
 
 end
